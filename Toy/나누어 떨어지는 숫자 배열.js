565a06a1ed6b424276a308bc1e3c4f853d57017f
HYO00 <79884004+HYO00@users.noreply.github.com>
2022-01-28 19:04:20 +0900

    나누어 떨어지는 숫자 배열 프로그래머스 Level1

---
+++ Toy/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,30 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12910
+//array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+//divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+
+//filter
+function solution(arr, divisor) {
+   
+    const result = arr.filter((el)=> el % divisor === 0);
+ 
+    result.sort((a,b) => a-b);
+    
+    return result.length  === 0 ? [-1] : result;
+}
+
+//forEach
+
+function solution(arr, divisor) {
+    let result = [];
+    arr.forEach((el)=>{
+        if(el % divisor === 0){
+            result.push(el);
+        }
+    })
+    result.sort((a,b) => a-b);
+    
+    return result.length  === 0 ? [-1] : result;
+}
+
+
